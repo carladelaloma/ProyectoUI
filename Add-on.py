9de14f4c485a3b53e881e3fa16a08df48656d1ee
Carla De La Loma <144267369+carladelaloma@users.noreply.github.com>
2024-12-10 14:14:46 +0100

    Update and rename Add-on_MoverOrigen.py to Add-on.py
    Añadido el segundo Add-on: mover el origen del objecto al centro de las caras seleccionadas.

--- Add-on.py
+++ Add-on.py
@@ -10,7 +10,7 @@ bl_info = {
 }
 
 
-import bpy
+import bpy, bmesh
 
 def move_obj_to_worldorigin():
     for obj in bpy.context.selected_objects:
@@ -18,12 +18,42 @@ def move_obj_to_worldorigin():
         obj.location = (0, 0, 0)
         obj.select_set(True)
 
+def set_origin_in_selected():
+    bpy.ops.object.mode_set(mode = 'EDIT', toggle = False) 
+    bpy.ops.mesh.select_mode(type="VERT")
+
+    me = bpy.context.edit_object.data
+    
+    bm = bmesh.from_edit_mesh(me)
+
+    selected_edge = 0
+    for index, vert in enumerate(bm.verts):
+        if (vert.select == True) :
+            selected_edge = index
+
+    bm.verts[selected_edge]
+    
+    bpy.ops.view3d.snap_cursor_to_selected()
+    bpy.ops.object.editmode_toggle()
+    bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
+
+
+class MESH_OT_set_origin_in_selected(bpy.types.Operator):
+    """Colocar el origen en el medio de las dos caras seleccionadas"""
+    
+    bl_idname = "mesh.set_origin_in_selected"  # Para que sea único y lo pueda llamar desde el panel
+    bl_label = "En el centro de la seleccion"
+    bl_options = {"REGISTER", "UNDO"}
+    
+    def execute(self, context):
+        set_origin_in_selected()  # Llamar la función para mover al origen del mundo
+        return {"FINISHED"}
 
 class MESH_OT_move_to_worldorigin(bpy.types.Operator):
     """Mover el origen de un objeto al del mundo"""
     
     bl_idname = "mesh.move_to_worldorigin"  # Para que sea único y lo pueda llamar desde el panel
-    bl_label = "Mover al origen del mundo"
+    bl_label = "Al origen del mundo"
     bl_options = {"REGISTER", "UNDO"}
     
     def execute(self, context):
@@ -49,17 +79,24 @@ class TestPanel(bpy.types.Panel):
         row = layout.row()
         row.operator("mesh.move_to_worldorigin", icon="TRACKER")  # Referencia al operador creado
 
+        row2 = layout.row()
+        row2.label(text="Mover el origen", icon = "WORLD_DATA")
+        
+        row2 = layout.row()
+        row2.operator("mesh.set_origin_in_selected", icon="PROP_OFF")
 
 # REGISTRAR LA CLASE
 
 def register():
     bpy.utils.register_class(TestPanel)
     bpy.utils.register_class(MESH_OT_move_to_worldorigin)
+    bpy.utils.register_class(MESH_OT_set_origin_in_selected)
 
 
 def unregister():
     bpy.utils.unregister_class(TestPanel)
     bpy.utils.unregister_class(MESH_OT_move_to_worldorigin)
+    bpy.utils.unregister_class(MESH_OT_set_origin_in_selected)
 
 if __name__ == "__main__":
     register()
